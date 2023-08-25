81765ee678a7f7744b6e5227158d5eb1832d196f
Arman baig <armanbaig012@gmail.com>
2023-08-25 12:33:24 +0530

    removed routes

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -7,8 +6,4 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
-@app.route('/bye')
-def bye():
-    return "<p>Bye!</p>"
 
